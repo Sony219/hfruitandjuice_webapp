bba79bc7352db1e57b36f0d0b1832110584ddb5e
Sonel Dorvil <101153504+Sony219@users.noreply.github.com>
2022-12-11 10:28:02 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
-$con = mysqli_connect('localhost', 'root', '','Sosofruits');
+$con = mysqli_connect('localhost', 'Fruit', 'Dany12345678@','Sosofruits');
 
 // get the post records
 $txtName = $_POST['txtName'];
